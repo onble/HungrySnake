313b27c2d536cea3ba3efecd7c7fa82deebdc399
onble <62949816+onble@users.noreply.github.com>
2025-06-22 14:30:50 +0800

    11-脚本的一些变量声明与食物预制件制作

--- assets/scripts/Head.ts
+++ assets/scripts/Head.ts
@@ -1,8 +1,25 @@
-import { _decorator, Component, Node } from "cc";
+import { _decorator, CCFloat, CCInteger, Component, Node, Prefab } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("Head")
 export class Head extends Component {
+    @property(Array(Node))
+    public bodyArray: Node[] = [];
+
+    @property(Prefab)
+    public bodyPrefab: Prefab = null;
+
+    @property(Prefab)
+    public foodPrefab: Prefab = null;
+
+    @property(CCInteger)
+    bodyNum: number = 2;
+
+    @property(CCFloat)
+    bodyDistance: number = 50;
+
+    speed: number = 200;
+
     start() {}
 
     update(deltaTime: number) {}
